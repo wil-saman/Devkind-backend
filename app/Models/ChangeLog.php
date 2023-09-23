30dcb97cbdac21f788503f7595765f4e5ad37629
wil-saman <timothy.wilbert12@gmail.com>
2023-09-23 23:05:18 +1000

    Worked on creating the api route and controllers for changelog. Installing sanctum for authentication.

--- app/Models/ChangeLog.php
+++ app/Models/ChangeLog.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ChangeLog extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'userId',
+        'changedItem',
+        'oldValue',
+        'newValue'
+    ];
 }
