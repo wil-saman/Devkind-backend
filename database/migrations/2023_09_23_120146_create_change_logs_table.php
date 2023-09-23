1b00cca9fea49f8ccf9b64b0a876253465d27a25
wil-saman <timothy.wilbert12@gmail.com>
2023-09-23 22:18:51 +1000

    Created model for Changelog. Ran migration.

---
+++ database/migrations/2023_09_23_120146_create_change_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('change_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('userId');
+            $table->string('changedItem');            
+            $table->string('oldValue');
+            $table->string('newValue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('change_logs');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/products', function(){
+    return 'rpducts';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
