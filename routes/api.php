583257629cacf5a24b05a873f1f6add29beb4209
wil-saman <timothy.wilbert12@gmail.com>
2023-09-23 23:44:09 +1000

    Worked on Auth controllers and Protected routes.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChangeLogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,10 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('changelog', ChangeLogController::class);
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('changelog', ChangeLogController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 // Route::get('/changelog', [ChangeLogController::class, 'index']);
-// Route::post('/changelog', [ChangeLogController::class, 'index']);
+// Route::post('/changelog', [ChangeLogController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
